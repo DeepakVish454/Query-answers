f76cf72a49c4390006e8410d22ad2ad094773895
Deepak Vishwakarma <deepakvish454@gmail.com>
2024-06-28 15:23:06 +0530

    fix all issue from backend

--- rF/Frontend/src/main.jsx
+++ rF/Frontend/src/main.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Header from './Header/Header.jsx';
-import Component from './SearchBox/index.jsx';
+import Component from './page/index.jsx';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <SearchBox /> */}
-    {/* <Header /> */}
     <Component/>
   </React.StrictMode>,
 )
